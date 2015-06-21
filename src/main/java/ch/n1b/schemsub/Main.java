d44d28aa772284a56c60023659cee56a770feea3
Thomas Richner <thomasrichner+github@gmail.com>
2015-06-21 02:18:49 +0200

    added option for blockdata

--- src/main/java/ch/n1b/schemsub/Main.java
+++ src/main/java/ch/n1b/schemsub/Main.java
@@ -20,16 +20,31 @@ public class Main {
     public static void main(String[] args) {
         if (args.length < 3) {
             System.err.println("Too few arguments.");
-            System.out.println("Usage: schemli <old blockid> <new blockid> <input schematic(s)> ");
+            System.out.println("Usage: schemli <old blockid>[:<old blockdata>] <new blockid>[:<new " +
+                    "blockdata>] <input schematic" +
+                    "(s)> ");
             System.exit(1);
         }
 
         int oldBlockIdTmp = 0;
         int newBlockIdTmp = 0;
-
+        int oldBlockDataTmp = -1;
+        int newBlockDataTmp = 0;
         try {
-            oldBlockIdTmp = Integer.parseInt(args[0]);
-            newBlockIdTmp = Integer.parseInt(args[1]);
+            if(args[0].contains(":")) {
+                String[] block = args[0].split(":");
+                oldBlockIdTmp = Integer.parseInt(block[0]);
+                oldBlockDataTmp = Integer.parseInt(block[1]);
+            }else {
+                oldBlockIdTmp = Integer.parseInt(args[0]);
+            }
+            if(args[1].contains(":")) {
+                String[] block = args[1].split(":");
+                newBlockIdTmp = Integer.parseInt(block[0]);
+                newBlockDataTmp = Integer.parseInt(block[1]);
+            }else {
+                newBlockIdTmp = Integer.parseInt(args[1]);
+            }
         } catch (NumberFormatException e) {
             System.err.println("Cannot parse block ids: " + e.getMessage());
             e.printStackTrace();
@@ -38,12 +53,16 @@ public class Main {
 
         final int oldBlockId = oldBlockIdTmp;
         final int newBlockId = newBlockIdTmp;
+        final int oldBlockData = oldBlockDataTmp;
+        final int newBlockData = newBlockDataTmp;
 
         List<String> schematics = Arrays.asList(Arrays.copyOfRange(args, 2, args.length));
-        schematics.parallelStream().forEach(s -> replaceBlocks(new File(s),oldBlockId,newBlockId));
+        schematics.parallelStream().forEach(s -> replaceBlocks(new File(s),oldBlockId,newBlockId,
+                oldBlockData,newBlockData));
     }
 
-    private static void replaceBlocks(File infile, int oldBlockId, int newBlockId) {
+    private static void replaceBlocks(File infile, int oldBlockId, int newBlockId,int oldBlockData,int
+            newBlockData) {
         if(!infile.exists()){
             System.err.println("Cannot find file: " + infile.getName());
             System.exit(1);
@@ -64,8 +83,8 @@ public class Main {
             System.exit(1);
         }
 
-        System.out.println("Replacing " + oldBlockId + " with " + newBlockId);
-        cuboid = replace(cuboid, oldBlockId, newBlockId);
+        System.out.println("Replacing " + oldBlockId + ":" + oldBlockData+ " with " + newBlockId + ":" + newBlockData);
+        cuboid = replace(cuboid, oldBlockId, newBlockId,oldBlockData,newBlockData);
 
         try {
             System.out.println("Saving " + infile.getName());
@@ -79,15 +98,16 @@ public class Main {
         System.out.println("Done.");
     }
 
-    private static Cuboid replace(Cuboid cuboid, int oldBlockId, int newBlockId) {
+    private static Cuboid replace(Cuboid cuboid, int oldBlockId, int newBlockId,int oldBlockData,int
+            newBlockData) {
         for(int x=0;x<cuboid.getWidth();x++){
             for (int y = 0; y < cuboid.getHeight(); y++) {
                 for (int z = 0; z < cuboid.getLength(); z++) {
                     Vec3D vec = new Vec3D(x, y, z);
                     BaseBlock block = cuboid.getBlock(vec);
-                    if (block.getType() == oldBlockId) {
-                        block.setType(newBlockId);
-                        cuboid.setBlock(vec,block);
+                    if (block.getType() == oldBlockId && (oldBlockData < 0 || oldBlockData==block.getData())) {
+                        BaseBlock newBlock = new BaseBlock(newBlockId,newBlockData);
+                        cuboid.setBlock(vec,newBlock);
                     }
                 }
             }
